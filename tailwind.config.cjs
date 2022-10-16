537eb682442f9418f1c6a7e293386d2ae526a8a7
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-10-16 21:32:06 +0300

    added: testing buttons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -85,26 +85,38 @@ module.exports = {
       ],
     },
     extend: {
-      backgroundImage: (theme) => ({
-        screens: "url('/path-to/image.png')",
-      }),
-
-      colors: {
-        "accent": {
-          "50": "#E5E5FF",
-          "100": "#CCCCFF",
-          "200": "#9999FF",
-          "300": "#6666FF",
-          "400": "#3333FF",
-          "500": "#0000FF",
-          "600": "#0000CC",
-          "700": "#000099",
-          "800": "#000066",
-          "900": "#000033"
-        },
+      backgroundImage:   {
+        sequoia: "linear-gradient( 229.7deg, #fdfdfe 14.85 % , #ffbb88 20.61 % , #ff7cba 26.17 % , #887bff 31.93%, #03c7f6 46.54 % , #0f1014 54.36%)",
       },
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      colors: {
+        //Base Colors
+        moonlightBase: "#0F1014",
+        moonlightInterface: "#111216",
+        moonlightOverlay: "#131317",
+        moonlightSoft: "#43444D",
+        moonlightSlight: "#575861",
+        moonlightText: "#868690",
+        moonlightFocusLow: "#121216",
+        moonlightFocusMedium: "#1A1B1F",
+        moonlightFocusHigh: "#1F1F24",
+        //Moonlight Accents
+        moonlightWhite: "#fdfdfe",
+        moonlightStone: "#9898a6",
+        moonlightOrange: "#ffbb88",
+        moonlightPink: "#f58ee0",
+        moonlightIndigo: "#c58fff",
+        moonlightBlue: "#8eb6f5",
+        //Monochrome Accents
+        monochromeKashmir: "#626983",
+        monochromeSlate: "#7C829D",
+        monochromeLogan: "#999EB2",
+        monochromeCadet: "#B6BAC8",
+        monochromeCat: "#D3D5DE",
+        monochromeCloud: "#E2E4ED",
+      },
+       fontFamily: {
+        sans: ["Neue Haas Grotesk Text Pro", ...defaultTheme.fontFamily.sans],
+        display: ["Neue Power", ...defaultTheme.fontFamily.sans],
       },
     },
   },
